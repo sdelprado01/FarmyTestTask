d582347b6d1ff4fcf6b1cee958d275ed9c794b01
Sergio del Prado Agudo <sdelprado01@gmail.com>
2021-03-24 16:36:09 +0100

    Camera function working TODO: Save image Collection view ADDS: Fireabase login and data storage

--- app/src/main/java/com/example/farmytesttask/MainActivity.kt
+++ app/src/main/java/com/example/farmytesttask/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.farmytesttask
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         //Sleep to let the user see the splash screen
         Thread.sleep(1000)
@@ -13,6 +15,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-
 
 }
