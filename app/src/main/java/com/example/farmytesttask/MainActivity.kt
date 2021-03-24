fc3e823c26034c0aa2db7dc6511d4dc642632bf7
Sergio del Prado Agudo <sdelprado01@gmail.com>
2021-03-24 14:16:56 +0100

    First GUI version TODO: Camera function work, see and edit collection

--- app/src/main/java/com/example/farmytesttask/MainActivity.kt
+++ app/src/main/java/com/example/farmytesttask/MainActivity.kt
@@ -5,7 +5,14 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        //Sleep to let the user see the splash screen
+        Thread.sleep(1000)
+
+        setTheme(R.style.Theme_FarmyTestTask)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
 }
